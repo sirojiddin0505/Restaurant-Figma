b3304a04c1b01df719785bdb974d652d1fd2cae8
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-03-18 20:29:56 +0500

    commit:
    git clear
    
    ::

---
+++ script.js
@@ -0,0 +1,15 @@
+// setTimeout
+
+// let timeoutId = setTimeout(() => {
+//    console.log("javascript");
+// }, 3000);
+
+// setTimeout(() => {
+//     clearTimeout(timeoutId);
+//     console.log("timeout to'xtatildi");
+// }, 4000);
+
+// let clear = setInterval(() => {
+//     console.log("hello world!");
+// }, 1000)
+// clearInterval(clear)
